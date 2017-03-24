70c5a43051069fc3d0de9f186b53192f292a4f40
Xavi R. Pinteño <xavi@jrpinteno.me>
2017-03-24 23:05:24 +0100

    Initial Commit

---
+++ Swift-8/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-8
+//
+//  Created by Xavier R. Pinteño on 24/03/2017.
+//  Copyright © 2017 Xavier R. Pinteño. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

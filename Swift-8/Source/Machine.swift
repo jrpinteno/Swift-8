f731ceae0ae8391db868081677989faa430041e2
Xavi R. Pinteño <xavi@jrpinteno.me>
2017-04-01 19:06:53 +0200

    Execute arithmetic operations and increase pc

--- Swift-8/Source/Machine.swift
+++ Swift-8/Source/Machine.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Machine {
+struct Machine: CustomStringConvertible {
 	/**
 	 * Chip 8 has a 4k memory usually distributed as follows
 	 * [0x000 - 0x1FF] Font data
@@ -29,6 +29,10 @@ struct Machine {
 	var sp: UInt16
 
 	var stack: [UInt16]
+
+	var description: String {
+		return String(format: "pc: \(pc), registers: \(vRegisters)")
+	}
 
 	let fontSet: [UInt8] = [
 		0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
@@ -62,5 +66,62 @@ struct Machine {
 
 	mutating func loadRom(_ rom: ROM) {
 		memory.replaceSubrange(Int(pc) ... (Int(pc) + rom.bytes.count - 1), with: rom.bytes)
+	}
+
+	mutating func emulateCycle() -> Bool {
+		var decoded: Bool = true
+
+		// Fetch opcode
+		let opcode: Opcode = Opcode(highByte: memory[Int(pc)], lowByte: memory[Int(pc + 1)])
+
+		// Decode and execute opcode
+		switch (opcode.nib1, opcode.nib2, opcode.nib3, opcode.nib4) {
+			case (0x6, _, _, _): // 6XNN
+				vRegisters[Int(opcode.nib2)] = opcode.byteL
+
+			case (0x7, _, _, _): // 7XNN
+				let x: Int = Int(opcode.nib2)
+				vRegisters[x] += opcode.byteL
+
+			case (0x8, _, _, 0x4): // 8XY4
+				let x: Int = Int(opcode.nib2)
+				let y: Int = Int(opcode.nib3)
+
+				vRegisters[0xF] = (vRegisters[x] + vRegisters[y] > UInt8.max) ? 0x1 : 0x0
+				vRegisters[x] += vRegisters[y]
+
+			case (0x8, _, _, 0x5): // 8XY5
+				let x: Int = Int(opcode.nib2)
+				let y: Int = Int(opcode.nib3)
+
+				vRegisters[0xF] = (vRegisters[x] - vRegisters[y] < 0) ? 0x0 : 0x1
+				vRegisters[x] -= vRegisters[y]
+
+			case (0x8, _, _, 0x6): // 8XY6
+				let x: Int = Int(opcode.nib2)
+				vRegisters[0xF] = vRegisters[x] & 0x1
+				vRegisters[x] = vRegisters[x] >> 1
+
+			case (0x8, _, _, 0x7): // 8XY7
+				let x: Int = Int(opcode.nib2)
+				let y: Int = Int(opcode.nib3)
+
+				vRegisters[0xF] = (vRegisters[y] - vRegisters[x] < 0) ? 0x0 : 0x1
+				vRegisters[x] = vRegisters[y] - vRegisters[x]
+
+			case (0x8, _, _, 0xE): // 8XYE
+				let x: Int = Int(opcode.nib2)
+				vRegisters[0xF] = (vRegisters[x] >> 7) & 0x1
+				vRegisters[x] = vRegisters[x] << 1
+
+			default:
+				print("\(opcode) -> Not decoded yet")
+				decoded = false
+		}
+
+		// Increase program counter
+		pc += 2
+
+		return decoded
 	}
 }
